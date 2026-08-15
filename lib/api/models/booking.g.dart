5b4b3093deda9f5275e72bf59b0ebb2c56b75c5c
christianmeude <253706969+christianmeude@users.noreply.github.com>
2026-08-15 10:49:09 +0000

    chore: auto-regenerate API client from latest schema

--- lib/api/models/booking.g.dart
+++ lib/api/models/booking.g.dart
@@ -1,21 +1,26 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'data2.dart';
+part of 'booking.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-_Data2 _$Data2FromJson(Map<String, dynamic> json) => _Data2(
+_Booking _$BookingFromJson(Map<String, dynamic> json) => _Booking(
   id: (json['id'] as num?)?.toInt(),
   bookingReference: json['booking_reference'] as String?,
   userId: (json['user_id'] as num?)?.toInt(),
   customerName: json['customer_name'] as String?,
-  status: json['status'] as String?,
+  customerEmail: json['customer_email'] as String?,
+  customerPhone: json['customer_phone'] as String?,
+  pax: (json['pax'] as num?)?.toInt(),
   eventDate: json['event_date'] == null
       ? null
       : DateTime.parse(json['event_date'] as String),
+  eventTime: json['event_time'] as String?,
+  venueAddress: json['venue_address'] as String?,
   paymentMethod: json['payment_method'] as String?,
+  status: json['status'] as String?,
   package: json['package'] == null
       ? null
       : Package.fromJson(json['package'] as Map<String, dynamic>),
@@ -24,14 +29,19 @@ _Data2 _$Data2FromJson(Map<String, dynamic> json) => _Data2(
       .toList(),
 );
 
-Map<String, dynamic> _$Data2ToJson(_Data2 instance) => <String, dynamic>{
+Map<String, dynamic> _$BookingToJson(_Booking instance) => <String, dynamic>{
   'id': instance.id,
   'booking_reference': instance.bookingReference,
   'user_id': instance.userId,
   'customer_name': instance.customerName,
-  'status': instance.status,
+  'customer_email': instance.customerEmail,
+  'customer_phone': instance.customerPhone,
+  'pax': instance.pax,
   'event_date': instance.eventDate?.toIso8601String(),
+  'event_time': instance.eventTime,
+  'venue_address': instance.venueAddress,
   'payment_method': instance.paymentMethod,
+  'status': instance.status,
   'package': instance.package,
   'scents': instance.scents,
 };
