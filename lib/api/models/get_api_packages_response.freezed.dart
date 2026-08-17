7f0244e5593e4f377172b7aedb2d9706e2e18d0d
christianmeude <253706969+christianmeude@users.noreply.github.com>
2026-08-17 02:43:50 +0000

    chore: auto-regenerate API client from latest schema

--- lib/api/models/get_api_packages_response.freezed.dart
+++ lib/api/models/get_api_packages_response.freezed.dart
@@ -1,6 +1,6 @@
-// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: type=lint
+// coverage:ignore-file
+// ignore_for_file: type=lint, type=warning, deprecated_member_use, deprecated_member_use_from_same_package
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'get_api_packages_response.dart';
@@ -9,179 +9,278 @@ part of 'get_api_packages_response.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// dart format off
 T _$identity<T>(T value) => value;
-
-final _privateConstructorUsedError = UnsupportedError(
-  'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models',
-);
-
-GetApiPackagesResponse _$GetApiPackagesResponseFromJson(
-  Map<String, dynamic> json,
-) {
-  return _GetApiPackagesResponse.fromJson(json);
-}
 
 /// @nodoc
 mixin _$GetApiPackagesResponse {
-  List<Package>? get data => throw _privateConstructorUsedError;
+
+ List<Package>? get data;
+/// Create a copy of GetApiPackagesResponse
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$GetApiPackagesResponseCopyWith<GetApiPackagesResponse> get copyWith => _$GetApiPackagesResponseCopyWithImpl<GetApiPackagesResponse>(this as GetApiPackagesResponse, _$identity);
 
   /// Serializes this GetApiPackagesResponse to a JSON map.
-  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  Map<String, dynamic> toJson();
 
-  /// Create a copy of GetApiPackagesResponse
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  $GetApiPackagesResponseCopyWith<GetApiPackagesResponse> get copyWith =>
-      throw _privateConstructorUsedError;
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is GetApiPackagesResponse&&const DeepCollectionEquality().equals(other.data, data));
+}
+
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,const DeepCollectionEquality().hash(data));
+
+@override
+String toString() {
+  return 'GetApiPackagesResponse(data: $data)';
+}
+
+
 }
 
 /// @nodoc
-abstract class $GetApiPackagesResponseCopyWith<$Res> {
-  factory $GetApiPackagesResponseCopyWith(
-    GetApiPackagesResponse value,
-    $Res Function(GetApiPackagesResponse) then,
-  ) = _$GetApiPackagesResponseCopyWithImpl<$Res, GetApiPackagesResponse>;
-  @useResult
-  $Res call({List<Package>? data});
-}
+abstract mixin class $GetApiPackagesResponseCopyWith<$Res>  {
+  factory $GetApiPackagesResponseCopyWith(GetApiPackagesResponse value, $Res Function(GetApiPackagesResponse) _then) = _$GetApiPackagesResponseCopyWithImpl;
+@useResult
+$Res call({
+ List<Package>? data
+});
 
+
+
+
+}
 /// @nodoc
-class _$GetApiPackagesResponseCopyWithImpl<
-  $Res,
-  $Val extends GetApiPackagesResponse
->
+class _$GetApiPackagesResponseCopyWithImpl<$Res>
     implements $GetApiPackagesResponseCopyWith<$Res> {
-  _$GetApiPackagesResponseCopyWithImpl(this._value, this._then);
+  _$GetApiPackagesResponseCopyWithImpl(this._self, this._then);
 
-  // ignore: unused_field
-  final $Val _value;
-  // ignore: unused_field
-  final $Res Function($Val) _then;
+  final GetApiPackagesResponse _self;
+  final $Res Function(GetApiPackagesResponse) _then;
 
-  /// Create a copy of GetApiPackagesResponse
-  /// with the given fields replaced by the non-null parameter values.
-  @pragma('vm:prefer-inline')
-  @override
-  $Res call({Object? data = freezed}) {
-    return _then(
-      _value.copyWith(
-            data: freezed == data
-                ? _value.data
-                : data // ignore: cast_nullable_to_non_nullable
-                      as List<Package>?,
-          )
-          as $Val,
-    );
-  }
+/// Create a copy of GetApiPackagesResponse
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? data = freezed,}) {
+  return _then(GetApiPackagesResponse(
+data: freezed == data ? _self.data : data // ignore: cast_nullable_to_non_nullable
+as List<Package>?,
+  ));
 }
 
-/// @nodoc
-abstract class _$$GetApiPackagesResponseImplCopyWith<$Res>
-    implements $GetApiPackagesResponseCopyWith<$Res> {
-  factory _$$GetApiPackagesResponseImplCopyWith(
-    _$GetApiPackagesResponseImpl value,
-    $Res Function(_$GetApiPackagesResponseImpl) then,
-  ) = __$$GetApiPackagesResponseImplCopyWithImpl<$Res>;
-  @override
-  @useResult
-  $Res call({List<Package>? data});
 }
 
-/// @nodoc
-class __$$GetApiPackagesResponseImplCopyWithImpl<$Res>
-    extends
-        _$GetApiPackagesResponseCopyWithImpl<$Res, _$GetApiPackagesResponseImpl>
-    implements _$$GetApiPackagesResponseImplCopyWith<$Res> {
-  __$$GetApiPackagesResponseImplCopyWithImpl(
-    _$GetApiPackagesResponseImpl _value,
-    $Res Function(_$GetApiPackagesResponseImpl) _then,
-  ) : super(_value, _then);
 
-  /// Create a copy of GetApiPackagesResponse
-  /// with the given fields replaced by the non-null parameter values.
-  @pragma('vm:prefer-inline')
-  @override
-  $Res call({Object? data = freezed}) {
-    return _then(
-      _$GetApiPackagesResponseImpl(
-        data: freezed == data
-            ? _value._data
-            : data // ignore: cast_nullable_to_non_nullable
-                  as List<Package>?,
-      ),
-    );
-  }
+/// Adds pattern-matching-related methods to [GetApiPackagesResponse].
+extension GetApiPackagesResponsePatterns on GetApiPackagesResponse {
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>(TResult Function( _GetApiPackagesResponse value)?  $default,{required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case _GetApiPackagesResponse() when $default != null:
+return $default(_that);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult map<TResult extends Object?>(TResult Function( _GetApiPackagesResponse value)  $default,){
+final _that = this;
+switch (_that) {
+case _GetApiPackagesResponse():
+return $default(_that);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>(TResult? Function( _GetApiPackagesResponse value)?  $default,){
+final _that = this;
+switch (_that) {
+case _GetApiPackagesResponse() when $default != null:
+return $default(_that);case _:
+  return null;
+
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( List<Package>? data)?  $default,{required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case _GetApiPackagesResponse() when $default != null:
+return $default(_that.data);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( List<Package>? data)  $default,) {final _that = this;
+switch (_that) {
+case _GetApiPackagesResponse():
+return $default(_that.data);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( List<Package>? data)?  $default,) {final _that = this;
+switch (_that) {
+case _GetApiPackagesResponse() when $default != null:
+return $default(_that.data);case _:
+  return null;
+
+}
+}
+
 }
 
 /// @nodoc
 @JsonSerializable()
-class _$GetApiPackagesResponseImpl implements _GetApiPackagesResponse {
-  const _$GetApiPackagesResponseImpl({final List<Package>? data})
-    : _data = data;
 
-  factory _$GetApiPackagesResponseImpl.fromJson(Map<String, dynamic> json) =>
-      _$$GetApiPackagesResponseImplFromJson(json);
+class _GetApiPackagesResponse implements GetApiPackagesResponse {
+  const _GetApiPackagesResponse({ List<Package>? data}): _data = data;
+  factory _GetApiPackagesResponse.fromJson(Map<String, dynamic> json) => _$GetApiPackagesResponseFromJson(json);
 
-  final List<Package>? _data;
-  @override
-  List<Package>? get data {
-    final value = _data;
-    if (value == null) return null;
-    if (_data is EqualUnmodifiableListView) return _data;
-    // ignore: implicit_dynamic_type
-    return EqualUnmodifiableListView(value);
-  }
-
-  @override
-  String toString() {
-    return 'GetApiPackagesResponse(data: $data)';
-  }
-
-  @override
-  bool operator ==(Object other) {
-    return identical(this, other) ||
-        (other.runtimeType == runtimeType &&
-            other is _$GetApiPackagesResponseImpl &&
-            const DeepCollectionEquality().equals(other._data, _data));
-  }
-
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  @override
-  int get hashCode =>
-      Object.hash(runtimeType, const DeepCollectionEquality().hash(_data));
-
-  /// Create a copy of GetApiPackagesResponse
-  /// with the given fields replaced by the non-null parameter values.
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  @override
-  @pragma('vm:prefer-inline')
-  _$$GetApiPackagesResponseImplCopyWith<_$GetApiPackagesResponseImpl>
-  get copyWith =>
-      __$$GetApiPackagesResponseImplCopyWithImpl<_$GetApiPackagesResponseImpl>(
-        this,
-        _$identity,
-      );
-
-  @override
-  Map<String, dynamic> toJson() {
-    return _$$GetApiPackagesResponseImplToJson(this);
-  }
+ final  List<Package>? _data;
+@override List<Package>? get data {
+  final value = _data;
+  if (value == null) return null;
+  if (_data is EqualUnmodifiableListView) return _data;
+  // ignore: implicit_dynamic_type
+  return EqualUnmodifiableListView(value);
 }
 
-abstract class _GetApiPackagesResponse implements GetApiPackagesResponse {
-  const factory _GetApiPackagesResponse({final List<Package>? data}) =
-      _$GetApiPackagesResponseImpl;
 
-  factory _GetApiPackagesResponse.fromJson(Map<String, dynamic> json) =
-      _$GetApiPackagesResponseImpl.fromJson;
+/// Create a copy of GetApiPackagesResponse
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$GetApiPackagesResponseCopyWith<_GetApiPackagesResponse> get copyWith => __$GetApiPackagesResponseCopyWithImpl<_GetApiPackagesResponse>(this, _$identity);
 
-  @override
-  List<Package>? get data;
-
-  /// Create a copy of GetApiPackagesResponse
-  /// with the given fields replaced by the non-null parameter values.
-  @override
-  @JsonKey(includeFromJson: false, includeToJson: false)
-  _$$GetApiPackagesResponseImplCopyWith<_$GetApiPackagesResponseImpl>
-  get copyWith => throw _privateConstructorUsedError;
+@override
+Map<String, dynamic> toJson() {
+  return _$GetApiPackagesResponseToJson(this, );
 }
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _GetApiPackagesResponse&&const DeepCollectionEquality().equals(other._data, _data));
+}
+
+@JsonKey(includeFromJson: false, includeToJson: false)
+@override
+int get hashCode => Object.hash(runtimeType,const DeepCollectionEquality().hash(_data));
+
+@override
+String toString() {
+  return 'GetApiPackagesResponse(data: $data)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$GetApiPackagesResponseCopyWith<$Res> implements $GetApiPackagesResponseCopyWith<$Res> {
+  factory _$GetApiPackagesResponseCopyWith(_GetApiPackagesResponse value, $Res Function(_GetApiPackagesResponse) _then) = __$GetApiPackagesResponseCopyWithImpl;
+@override @useResult
+$Res call({
+ List<Package>? data
+});
+
+
+
+
+}
+/// @nodoc
+class __$GetApiPackagesResponseCopyWithImpl<$Res>
+    implements _$GetApiPackagesResponseCopyWith<$Res> {
+  __$GetApiPackagesResponseCopyWithImpl(this._self, this._then);
+
+  final _GetApiPackagesResponse _self;
+  final $Res Function(_GetApiPackagesResponse) _then;
+
+/// Create a copy of GetApiPackagesResponse
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? data = freezed,}) {
+  return _then(_GetApiPackagesResponse(
+data: freezed == data ? _self._data : data // ignore: cast_nullable_to_non_nullable
+as List<Package>?,
+  ));
+}
+
+
+}
+
+// dart format on
