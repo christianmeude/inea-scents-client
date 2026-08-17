8edfa5f57100bbfe3f6264464394d773a7fc5882
Christian Meude <christianmeude17@gmail.com>
2026-08-17 09:50:08 +0800

    ci: automate api schema fetching via github action

--- scripts/generate_openapi_client.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-OPENAPI_URL=${1:-https://inea-scents.onrender.com/docs?api-docs.json}
-OUT_DIR=${2:-lib/api}
-GENERATOR=${3:-dart-dio}
-
-echo "Generating OpenAPI client from $OPENAPI_URL into $OUT_DIR using $GENERATOR"
-mkdir -p "$OUT_DIR"
-openapi-generator-cli generate -i "$OPENAPI_URL" -g "$GENERATOR" -o "$OUT_DIR" --additional-properties=supportsEnumerations=true
-echo "Done. Run 'flutter pub run build_runner build' if using freezed/json_serializable on generated code."
